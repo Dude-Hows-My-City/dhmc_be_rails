50d224bd594fb242dea122f20f8f4384ae051392
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-27 15:01:28 -0600

    Test: City model & validations

---
+++ spec/models/city_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe City do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:full_name) }
+    it { should validate_presence_of(:summary) }
+    it { should validate_presence_of(:scores) }
+    it { should validate_presence_of(:details) }
+  end
+end
