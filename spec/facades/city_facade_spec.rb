65c69224833d304dc6707034bbcc3d6400fcba50
Zachary Hazelwood <zhaze001@gmail.com>
2022-08-28 16:31:58 -0600

    Test: City Facade update

--- spec/facades/city_facade_spec.rb
+++ spec/facades/city_facade_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe CityFacade do
   it "creates city objects from multiple endpoints" do
+    City.destroy_all
     cities = CityFacade.create_city_objects
-require "pry"; binding.pry
-    expect(cities).to be_all City
+
+    expect(cities).to be_all UrbanArea # Test saves and returns PORO
+    expect(City.all.count).to eq(266)
   end
 end
