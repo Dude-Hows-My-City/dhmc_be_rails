bcb4db24dad2aa902440c63927a4a2579f29322c
Zachary P <65498038+z-prince@users.noreply.github.com>
2022-08-29 17:42:10 -0400

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:user_name) }
   end
 end
