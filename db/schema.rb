1d7d73151059befe2cb7f2fd71f0509b3c8563ee
Zach Prince <zachary.s.prince@outlook.com>
2022-08-29 16:50:52 -0400

    Add search table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_27_194720) do
+ActiveRecord::Schema.define(version: 2022_08_29_205035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,15 @@ ActiveRecord::Schema.define(version: 2022_08_27_194720) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "urban_areas", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+  end
+
+  add_foreign_key "searches", "users"
 end
