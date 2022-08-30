74f0678d3b6bd49ac9139beb54c515fa4d068cf0
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-30 13:39:52 -0600

    add favorites table

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe City do
+  describe 'Relationships' do
+    it { should have_many :favorites }
+    it { should have_many(:users).through(:favorites) }
+  end
+
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:full_name) }
