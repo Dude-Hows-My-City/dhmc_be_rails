eb9178dbe5a67c3b629f1cba251f1d5b91a88720
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-30 00:35:50 -0700

    Test: add request spec for /api/v1/cities/{id}

--- spec/requests/api/v1/cities_request.rb
+++ spec/requests/api/v1/cities_request.rb
@@ -23,4 +23,19 @@ RSpec.describe 'Cities API' do
             expect(city[:attributes]).to have_key(:details)
         end 
     end
+
+    it "can return one city by its id" do
+        id = create(:city).id
+        get "/api/v1/cities/#{id}"
+
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        city = response_body[:data]
+        expect(response).to have_http_status(200)
+        expect(city[:type]).to eq("city")
+        expect(city[:attributes]).to have_key(:name)
+        expect(city[:attributes]).to have_key(:full_name)
+        expect(city[:attributes]).to have_key(:summary)
+        expect(city[:attributes]).to have_key(:scores)
+        expect(city[:attributes]).to have_key(:details)
+    end
 end
