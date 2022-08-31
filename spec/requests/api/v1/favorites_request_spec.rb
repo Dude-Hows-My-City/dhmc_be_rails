91ca9fce4dace663c6f22859e6d0808aac59ccae
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-30 23:22:31 -0700

    Test: add get user favorites endpoint

--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe 'Favorites Request Endpoint' do
         
         expect(user.cities.first.name).to eq(city.name)
     end
+    
+    it "displays a list of user favorite cities" do
+        user = User.create!(username: 'Tony')
+        city_list = create_list(:city, 5)
+        city_1 = city_list.first
+        city_3 = city_list.third
+        city_5 = city_list.fifth
+        
+        Favorite.create(user_id: user.id, city_id: city_1.id)
+        Favorite.create(user_id: user.id, city_id: city_3.id)
+        Favorite.create(user_id: user.id, city_id: city_5.id)
+        
+        get "/api/v1/favorites/#{user.id}"
+
+        expect(response).to  be_successful
+        expect(response).to  have_http_status(200)
+        response_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_body[:data].first[:id]).to eq(city_1.id.to_s)
+        expect(response_body[:data].second[:id]).to eq(city_3.id.to_s)
+        expect(response_body[:data].third[:id]).to eq(city_5.id.to_s)
+    end
 end
