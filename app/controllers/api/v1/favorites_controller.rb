30c85e4be477e49285f1cc0bb42b4fe5a91e48db
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-30 23:23:22 -0700

    Add: index action to favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::FavoritesController < ApplicationController 
+    def index
+        user = User.find(params[:id])
+        render json: CitySerializer.new(user.cities)
+    end
+    
     def create
         user_id = params[:favorite][:user_id]
         city_id = params[:favorite][:city_id]
