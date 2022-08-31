d1e5ffb9c7bbe9479b05e12b6070275da4b9b284
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-30 23:44:11 -0700

    Add: destroy action to favorite controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -9,4 +9,12 @@ class Api::V1::FavoritesController < ApplicationController
         city_id = params[:favorite][:city_id]
         Favorite.create(user_id: user_id, city_id: city_id) 
     end
+
+    def destroy
+        user = User.find(params[:user_id])
+        favorite = user.favorites.where(city_id: params[:city_id]).first
+        Favorite.delete(favorite.id)
+        head :no_content
+    end
+    
 end
