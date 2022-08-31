99753045103dc6b01465dd900a88975ddb88a13b
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-30 22:32:52 -0700

    Add: post favorites endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cities, only: [:index, :show]
+      resources :favorites, only: [:create]
     end
   end
 
