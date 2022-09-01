41621d50fbcccfbc97cd98d572426a5f61d85567
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-31 21:39:35 -0700

    Add: sad path error response for city request

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -4,6 +4,16 @@ class Api::V1::CitiesController < ApplicationController
     end
 
     def show
-        render json: CitySerializer.new(City.find(params[:id]))
+        if City.where(id: params[:id]).first.class == City
+            render json: CitySerializer.new(City.find(params[:id]))
+        else
+            invalid_city_identification
+        end
     end
+
+     private
+
+    def invalid_city_identification
+        render json: { data: { message: 'City ID does not exist' }}, status: 400
+    end 
 end
