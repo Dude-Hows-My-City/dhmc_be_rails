8b3c13709f11afd061ec188a4578f3b98c4e5e97
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-31 21:39:07 -0700

    Test: sad path for invalid city request

--- spec/requests/api/v1/cities_request_spec.rb
+++ spec/requests/api/v1/cities_request_spec.rb
@@ -1,41 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'Cities Request Endpoint' do
-    it "returns a list of cities" do
-        create_list(:city, 5)
-        get '/api/v1/cities'
-        
-        expect(response).to  be_successful
-        expect(response).to  have_http_status(200)
-
-        response_body = JSON.parse(response.body, symbolize_names: true)
-        cities = response_body[:data]
-
-        expect(cities.count).to eq(5) 
-        cities.each do |city|
-            expect(city).to have_key(:id)
-            expect(city).to have_key(:type)
-            expect(city).to have_key(:attributes)
+    describe "happy path" do
+        it "returns a list of cities" do
+            create_list(:city, 5)
+            get '/api/v1/cities'
+            
+            expect(response).to  be_successful
+            expect(response).to  have_http_status(200)
+    
+            response_body = JSON.parse(response.body, symbolize_names: true)
+            cities = response_body[:data]
+    
+            expect(cities.count).to eq(5) 
+            cities.each do |city|
+                expect(city).to have_key(:id)
+                expect(city).to have_key(:type)
+                expect(city).to have_key(:attributes)
+                expect(city[:attributes]).to have_key(:name)
+                expect(city[:attributes]).to have_key(:full_name)
+                expect(city[:attributes]).to have_key(:summary)
+                expect(city[:attributes]).to have_key(:scores)
+                expect(city[:attributes]).to have_key(:details)
+            end 
+        end
+    
+        it "can return one city by its id" do
+            id = create(:city).id
+            get "/api/v1/cities/#{id}"
+    
+            response_body = JSON.parse(response.body, symbolize_names: true)
+            city = response_body[:data]
+            expect(response).to have_http_status(200)
+            expect(city[:type]).to eq("city")
             expect(city[:attributes]).to have_key(:name)
             expect(city[:attributes]).to have_key(:full_name)
             expect(city[:attributes]).to have_key(:summary)
             expect(city[:attributes]).to have_key(:scores)
             expect(city[:attributes]).to have_key(:details)
-        end 
+        end
     end
+    
+    describe "sad path" do
+        it "returns an error if id isnt valid" do
+            get "/api/v1/cities/500"
+            expect(response).to have_http_status(400)
 
-    it "can return one city by its id" do
-        id = create(:city).id
-        get "/api/v1/cities/#{id}"
-
-        response_body = JSON.parse(response.body, symbolize_names: true)
-        city = response_body[:data]
-        expect(response).to have_http_status(200)
-        expect(city[:type]).to eq("city")
-        expect(city[:attributes]).to have_key(:name)
-        expect(city[:attributes]).to have_key(:full_name)
-        expect(city[:attributes]).to have_key(:summary)
-        expect(city[:attributes]).to have_key(:scores)
-        expect(city[:attributes]).to have_key(:details)
+            error = JSON.parse(response.body, symbolize_names: true)
+            
+            expect(error).to have_key(:data)
+            expect(error[:data][:message]).to eq("City ID does not exist")
+        end
     end
+    
 end
