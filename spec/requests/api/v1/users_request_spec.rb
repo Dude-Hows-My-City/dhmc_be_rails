b1d286d306adfa770fa73224ed67d7e3e40ee9b5
Stephen Wilkens <stephenwilkens@gmail.com>
2022-09-03 16:36:02 -0600

    complete happy path test

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe 'Users Create Endpoint' do
       user_params = '{ "username":"Hotdog King" }'
       headers = { "CONTENT_TYPE" => "application/json" }
       post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
-# require "pry"; binding.pry
       expect(response).to  be_successful
       expect(response).to  have_http_status(200)
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to have_key(:data)
+      expect(response_body[:data]).to have_key(:id)
+      expect(response_body[:data]).to have_key(:type)
+      expect(response_body[:data]).to have_key(:attributes)
+      expect(response_body[:data][:attributes]).to have_key(:username)
     end
   end
 end
